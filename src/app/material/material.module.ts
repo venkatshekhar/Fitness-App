af3560af364f2fcbbdbbe28d001ed73c0f375803
Shekahr shetty <vmuthams@ciena.com>
2022-04-11 20:38:47 +0530

    initial project setup and signup page design

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
+  ]
+})
+export class MaterialModule { }
