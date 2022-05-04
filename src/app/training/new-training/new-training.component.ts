f7b0344c338fcd4680b3aa0ed5877efbaf0db02e
Shekahr shetty <vmuthams@ciena.com>
2022-05-04 07:20:25 +0530

    read all excercises from firestore

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Excercise } from '../excercise.model';
 import { TrainingService } from '../training.service';
 import { NgForm } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-new-training',
@@ -10,13 +12,14 @@ import { NgForm } from '@angular/forms';
 })
 export class NewTrainingComponent implements OnInit {
 
-  excercises : Excercise[]=[];
+  excercises : Observable<any>;
 
 
-  constructor(private trainingService : TrainingService) { }
+  constructor(private trainingService : TrainingService, private fireDB: AngularFirestore) { }
 
   ngOnInit(): void {
-    this.excercises= this.trainingService.getAvailableExcercises();
+    // this.excercises= this.trainingService.getAvailableExcercises();
+    this.excercises = this.fireDB.collection('availableExercises').valueChanges();
   }
 
   onStartTraining(form: NgForm){
