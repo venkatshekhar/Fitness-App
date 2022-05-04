f5526e7546ec8669de5da330ac9f470a20049697
Shekahr shetty <vmuthams@ciena.com>
2022-05-04 08:06:26 +0530

    use snapshot to get metadata transform data using map

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -4,7 +4,7 @@ import { TrainingService } from '../training.service';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore'
 import { Observable } from 'rxjs';
-
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
@@ -12,17 +12,27 @@ import { Observable } from 'rxjs';
 })
 export class NewTrainingComponent implements OnInit {
 
-  excercises : Observable<any>;
+  excercises: Observable<Excercise[]>;
 
 
-  constructor(private trainingService : TrainingService, private fireDB: AngularFirestore) { }
+  constructor(private trainingService: TrainingService, private fireDB: AngularFirestore) { }
 
   ngOnInit(): void {
     // this.excercises= this.trainingService.getAvailableExcercises();
-    this.excercises = this.fireDB.collection('availableExercises').valueChanges();
+    this.excercises = this.fireDB.collection('availableExercises').snapshotChanges().pipe(map((docArray: any[]) => {
+      return docArray.map(doc => {
+        return {
+          id: doc.payload.doc.id,
+          ...doc.payload.doc.data()
+        }
+      });
+    }))
+    // .subscribe(result =>{
+    //   console.log('result data :', result)
+    // });
   }
 
-  onStartTraining(form: NgForm){
+  onStartTraining(form: NgForm) {
     this.trainingService.startExcercise(form.value.exercise);
   }
 
