3be9f9104ceb73e214b8fae92200ad00aa1bce33
Shekahr shetty <vmuthams@ciena.com>
2022-05-10 07:33:04 +0530

    production build changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBglNfU6qvCkBf2l83oiSy3c8lXrB5WDgc",
+    authDomain: "venky-fitness-tracking.firebaseapp.com",
+    projectId: "venky-fitness-tracking",
+    storageBucket: "venky-fitness-tracking.appspot.com",
+    databaseURL: "https://venky-fitness-tracking.firebaseio.com",
+    messagingSenderId: "213156832837",
+    appId: "1:213156832837:web:7c989e316e91cf2a1b652c",
+    measurementId: "G-TELLX90R9Z"
+  }
 };
